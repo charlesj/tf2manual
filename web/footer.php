37f26fdb654afcbb8eb6aad2a5a5496663de550e
Josh Charles <josh.charles@gmail.com>
2013-06-03 19:57:53 -0700

    Clean up

--- web/footer.php
+++
@@ -1,5 +0,0 @@
-		<div id="footer">
-			The Missing Team Fortress 2 Manual by the Reddit Team Fortress 2 Community is licensed under a Creative Commons Attribution 3.0 United States License.
-		</div>
-	</body>
-</html>
